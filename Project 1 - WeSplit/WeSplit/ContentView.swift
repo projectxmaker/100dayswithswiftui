fc06827d1fbd1c7ea97089cede8bd3400688d5fd
Tuan <projectxmaker@gmail.com>
2022-09-24 10:51:02 +0700

    add button w/ @State for its tap count

--- Project 1 - WeSplit/WeSplit/ContentView.swift
+++ Project 1 - WeSplit/WeSplit/ContentView.swift
@@ -8,11 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0
+    
     var body: some View {
         NavigationView {
             Form {
                 Section {
                     Text("Hello, world!")
+                }
+                Button("Tap Count: \(tapCount)") {
+                    tapCount += 1
                 }
             }
             .navigationTitle("SwiftUI")
