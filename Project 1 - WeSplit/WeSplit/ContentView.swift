fb2cdb60e31f307593a3217aa4310613b59eaf9b
Tuan <projectxmaker@gmail.com>
2022-09-24 11:03:43 +0700

    add TextField w/ 2 way binding by adding a prefix of $ to variable name

--- Project 1 - WeSplit/WeSplit/ContentView.swift
+++ Project 1 - WeSplit/WeSplit/ContentView.swift
@@ -9,12 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var tapCount = 0
+    @State private var name = ""
     
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    Text("Hello, world!")
+                    TextField("Enter your name", text: $name)
+                    Text("Your name is \(name)")
                 }
                 Button("Tap Count: \(tapCount)") {
                     tapCount += 1
