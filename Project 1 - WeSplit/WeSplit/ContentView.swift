df30f08fcbf259cdf4c3d91077f658681956e26a
Tuan <projectxmaker@gmail.com>
2022-09-24 11:23:31 +0700

    add Picker w/ content that is made by ForEach viewType which helps to create an array of views

--- Project 1 - WeSplit/WeSplit/ContentView.swift
+++ Project 1 - WeSplit/WeSplit/ContentView.swift
@@ -11,9 +11,18 @@ struct ContentView: View {
     @State private var tapCount = 0
     @State private var name = ""
     
+    let students = ["Harry", "Harmony", "Ron"]
+    @State private var selectedStudent = "Harry"
+    
     var body: some View {
         NavigationView {
             Form {
+                Picker("Select your studen", selection: $selectedStudent) {
+                    ForEach(students, id: \.self) { student in
+                        Text(student)
+                    }
+                }
+                
                 Section {
                     TextField("Enter your name", text: $name)
                     Text("Your name is \(name)")
