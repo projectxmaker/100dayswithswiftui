856698df9ae341761f1f15142e24faba500477a5
Tuan <projectxmaker@gmail.com>
2022-09-27 10:43:17 +0700

    enhance text w/ font size & weight, enhance display of button-based flags w/ shadow and shape

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -31,16 +31,18 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            Color.blue
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             
             VStack(spacing: 30) {
                 VStack {
                     Text("Tap the flag of")
                         .foregroundColor(.white)
+                        .font(.subheadline.weight(.heavy))
 
                     Text(countries[correctAnswer])
                         .foregroundColor(.white)
+                        .font(.largeTitle.weight(.semibold))
                 }
 
                 ForEach(0..<3) { number in
@@ -49,6 +51,8 @@ struct ContentView: View {
                     } label: {
                         Image(countries[number])
                             .renderingMode(.original)
+                            .clipShape(Capsule())
+                            .shadow(color: .white, radius: 5, x: 0, y: 2)
                     }
                 }
             }
