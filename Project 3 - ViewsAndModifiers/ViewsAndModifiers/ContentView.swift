7e1a3df5e758a1e4d3e0086a40a76c7f57e07fa2
Tuan <projectxmaker@gmail.com>
2022-09-28 10:07:26 +0700

    Init code for project 3

---
+++ Project 3 - ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by Pham Anh Tuan on 9/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project 3 - ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Pham Anh Tuan on 9/28/22.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
