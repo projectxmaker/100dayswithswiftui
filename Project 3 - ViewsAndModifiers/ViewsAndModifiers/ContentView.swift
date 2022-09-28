da2d256607e072e5ecee5df4bb183ad69b725699
Tuan <projectxmaker@gmail.com>
2022-09-28 16:03:43 +0700

    Challenge 3 of project 3: create new View Modifier which will be a modifier of View and format text in View w/ large font and blue color

--- Project 3 - ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Project 3 - ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -12,10 +12,19 @@ struct ContentView: View {
     @State private var something = 0
     
     var body: some View {
-        Color.blue
-            .frame(width: 300, height: 200)
-            .watermarked(with: "Hacking with Swift")
-        
+        VStack {
+            Spacer()
+            
+            Section {
+                Text("Text WITHOUT Large Blue Font Modifier")
+                Text("Text WITH Large Blue Font Modifier")
+                    .largeBlueFont()
+            }
+            .multilineTextAlignment(.center)
+            
+            Spacer()
+            Spacer()
+        }
     }
 }
 
@@ -25,23 +34,16 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct Watermark: ViewModifier {
-    var text: String
-
+struct LargeBlueFont: ViewModifier {
     func body(content: Content) -> some View {
-        ZStack(alignment: .bottomTrailing) {
-            content
-            Text(text)
-                .font(.caption)
-                .foregroundColor(.white)
-                .padding(5)
-                .background(.black)
-        }
+        content
+            .foregroundColor(.blue)
+            .font(.largeTitle)
     }
 }
 
 extension View {
-    func watermarked(with text: String) -> some View {
-        modifier(Watermark(text: text))
+    func largeBlueFont() -> some View {
+        modifier(LargeBlueFont())
     }
 }
