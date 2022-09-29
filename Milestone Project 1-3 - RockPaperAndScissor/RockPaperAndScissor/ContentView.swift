f5e5ad3700d2ce30ba351d9aa010b504896e7bcd
Tuan <projectxmaker@gmail.com>
2022-09-29 08:25:44 +0700

    create 3 buttons for rock, paper and scissor

--- Milestone Project 1-3 - RockPaperAndScissor/RockPaperAndScissor/ContentView.swift
+++ Milestone Project 1-3 - RockPaperAndScissor/RockPaperAndScissor/ContentView.swift
@@ -7,15 +7,57 @@
 
 import SwiftUI
 
+enum ResultStatus: String {
+    case botWin = "Win"
+    case botLose = "Lose"
+}
+
 struct ContentView: View {
+    @State private var items: [String] = [
+        "✊",
+        "🖐",
+        "✌️",
+    ]
+    @State private var userChoice = ""
+    
+    @State private var itemOfBot: Int = 0
+    @State private var resultStatus = ResultStatus.botWin
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ZStack {
+                LinearGradient(colors: [.blue, .white], startPoint: .top, endPoint: .bottom)
+                    .ignoresSafeArea()
+                
+                VStack {
+                    Spacer()
+                    
+                    Text(items[itemOfBot])
+                    Text(resultStatus.rawValue)
+                    
+                    Spacer()
+                    
+                    VStack (spacing: 20) {
+                        ForEach(items, id: \.self) { item in
+                            Button {
+                                userChoice = item
+                            } label: {
+                                Text(item)
+                                    .font(.system(size: 100))
+                                    .frame(width: 160, height: 160)
+                                    .background(RadialGradient(colors: [.red, .yellow, .blue], center: .center, startRadius: 20, endRadius: 100))
+                                    .clipShape(Capsule())
+                                    .shadow(color: .white, radius: 5, x: 0, y: 2)
+                            }
+                        }
+                    }
+                    
+                    Spacer()
+                    Spacer()
+                }
+            }
         }
-        .padding()
     }
 }
 
