574ddbc77a6fb863e0cbebcc9c637a90fa45d4a7
Tuan <projectxmaker@gmail.com>
2022-09-30 11:36:51 +0700

    Init code of project 4 w/ layout including 3 inputs & 1 button

---
+++ Project 4 - Better Rest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Pham Anh Tuan on 9/30/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Project 4 - Better Rest/BetterRest/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Pham Anh Tuan on 9/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
+    let tomorrow = Date.now.addingTimeInterval(86400)
+    
+    func calculateBedtime() {
+    }
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+
+                Text("Desired amount of sleep")
+                    .font(.headline)
+
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+
+                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+
+            }
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBedtime)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
