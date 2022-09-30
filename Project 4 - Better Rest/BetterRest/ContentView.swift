f2548bbc7dc0b04fe68d67ff661bbc74949bcef5
Tuan <projectxmaker@gmail.com>
2022-09-30 12:05:37 +0700

    import model, then produce the time need to go to the bed

--- Project 4 - Better Rest/BetterRest/ContentView.swift
+++ Project 4 - Better Rest/BetterRest/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by Pham Anh Tuan on 9/30/22.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
@@ -15,6 +16,21 @@ struct ContentView: View {
     let tomorrow = Date.now.addingTimeInterval(86400)
     
     func calculateBedtime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+
+            let sleepTime = wakeUp - prediction.actualSleep
+
+        } catch {
+            // something went wrong!
+        }
     }
     
     var body: some View {
