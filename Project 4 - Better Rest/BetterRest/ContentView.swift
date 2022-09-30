37d27c1c9ba6b6fc800ed5bbee4963a9c8c0cbd4
Tuan <projectxmaker@gmail.com>
2022-09-30 14:39:16 +0700

    set default wakeup time is 7:00 AM

--- Project 4 - Better Rest/BetterRest/ContentView.swift
+++ Project 4 - Better Rest/BetterRest/ContentView.swift
@@ -9,13 +9,20 @@ import CoreML
 import SwiftUI
 
 struct ContentView: View {
-    @State private var wakeUp = Date.now
+    @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     
     @State private var alertTitle = ""
     @State private var alertMessage = ""
     @State private var showingAlert = false
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
+    }
     
     let tomorrow = Date.now.addingTimeInterval(86400)
     
