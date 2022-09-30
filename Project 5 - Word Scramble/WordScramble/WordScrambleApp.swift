28b89eacb3b9c5ec8bb49469ec215ce644dda910
Tuan <projectxmaker@gmail.com>
2022-09-30 20:56:34 +0700

    Init code for project 5

---
+++ Project 5 - Word Scramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Pham Anh Tuan on 9/30/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
