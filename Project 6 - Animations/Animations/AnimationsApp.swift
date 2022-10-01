cb38d4903c11538d1438323906f4db7475fb9c0a
Tuan <projectxmaker@gmail.com>
2022-10-01 10:30:39 +0700

    Init code for project 6

---
+++ Project 6 - Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Pham Anh Tuan on 10/1/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
