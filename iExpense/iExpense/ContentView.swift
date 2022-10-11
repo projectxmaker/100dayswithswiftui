b3d429ecda3356d5c57501ba38734c34ee65a662
Tuan <projectxmaker@gmail.com>
2022-10-11 09:38:13 +0700

    add id to ExpenseItem and use id as unique value in ForEach

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ExpenseItem {
+    let id = UUID()
     let name: String
     let type: String
     let amount: Double
@@ -27,7 +28,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List {
-                ForEach(expenses.items, id: \.name) { item in
+                ForEach(expenses.items, id: \.id) { item in
                     Text(item.name)
                 }
                 .onDelete(perform: removeItems)
