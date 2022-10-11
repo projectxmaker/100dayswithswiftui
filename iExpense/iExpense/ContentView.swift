d87b1f743d911fcf9de57c06e3b89100e428fce9
Tuan <projectxmaker@gmail.com>
2022-10-11 09:42:15 +0700

    make ExpenseItem conforms to Identifiable protocol so don't need to specify id in ForEach

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ExpenseItem {
+struct ExpenseItem: Identifiable {
     let id = UUID()
     let name: String
     let type: String
@@ -28,7 +28,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List {
-                ForEach(expenses.items, id: \.id) { item in
+                ForEach(expenses.items) { item in
                     Text(item.name)
                 }
                 .onDelete(perform: removeItems)
