637c4d995110f657374c3e45d9a0a072a43adbad
Tuan <projectxmaker@gmail.com>
2022-10-11 10:26:11 +0700

    dismiss AddView after creating new ExpenseItem; show full information of each ExpenseItem of the List in ContentView

--- iExpense/iExpense/AddView.swift
+++ iExpense/iExpense/AddView.swift
@@ -13,6 +13,8 @@ struct AddView: View {
     @State private var amount = 0.0
     
     @ObservedObject var expenses: Expenses
+    
+    @Environment(\.dismiss) var dismiss
 
     let types = ["Business", "Personal"]
 
@@ -35,6 +37,8 @@ struct AddView: View {
                 Button("Save") {
                     let item = ExpenseItem(name: name, type: type, amount: amount)
                     expenses.items.append(item)
+                    
+                    dismiss()
                 }
             }
         }
--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -48,7 +48,16 @@ struct ContentView: View {
         NavigationView {
             List {
                 ForEach(expenses.items) { item in
-                    Text(item.name)
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+
+                        Spacer()
+                        Text(item.amount, format: .currency(code: "USD"))
+                    }
                 }
                 .onDelete(perform: removeItems)
             }
