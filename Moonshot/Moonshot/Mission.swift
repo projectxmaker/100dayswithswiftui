46202a3bcd889341836328406283d92770c6c32a
Tuan <projectxmaker@gmail.com>
2022-10-12 09:17:54 +0700

    improve decode method of Bundle extension w/ generic type so that it could be used to decode any type of JSON-based data

---
+++ Moonshot/Moonshot/Mission.swift
@@ -0,0 +1,20 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Pham Anh Tuan on 10/12/22.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
