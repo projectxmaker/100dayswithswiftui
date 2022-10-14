67b9b5f4be03a7150ce0abeed65b5d9b627b1ad4
Tuan <projectxmaker@gmail.com>
2022-10-14 18:00:49 +0700

    Challenge 3 of Project 9: allow the users to controll the grandient position

--- Drawing/Drawing/ColorCyclingRetangleView.swift
+++ Drawing/Drawing/ColorCyclingRetangleView.swift
@@ -7,14 +7,19 @@
 
 import SwiftUI
 
-struct ColorCyclingCircle: View {
+struct ColorCyclingRectangle: View {
     var amount = 0.0
-    var steps = 100
+    var startX: CGFloat = 0.0
+    var startY: CGFloat = 0.0
+    var endX: CGFloat = 0.0
+    var endY: CGFloat = 0.0
+    
+    let steps = 100
 
     var body: some View {
         ZStack {
             ForEach(0..<steps, id: \.self) { value in
-                Circle()
+                Rectangle()
                     .inset(by: Double(value))
                     .strokeBorder(
                         LinearGradient(
@@ -22,8 +27,8 @@ struct ColorCyclingCircle: View {
                                 color(for: value, brightness: 1),
                                 color(for: value, brightness: 0.5)
                             ]),
-                            startPoint: .top,
-                            endPoint: .bottom
+                            startPoint: UnitPoint(x: startX, y: startY),
+                            endPoint: UnitPoint(x: endX, y: endY)
                         ),
                         lineWidth: 2
                     )
@@ -43,15 +48,72 @@ struct ColorCyclingCircle: View {
     }
 }
 
+func createSeparator() -> some View {
+    Rectangle()
+        .frame(maxWidth: .infinity / 2, maxHeight: 2)
+        .background(.gray)
+        .opacity(0.1)
+}
+
 struct ColorCyclingRetangleView: View {
     @State private var colorCycle = 0.0
-
+    @State private var gradientStartX = 0.0
+    @State private var gradientStartY = 0.0
+    @State private var gradientEndX = 0.0
+    @State private var gradientEndY = 0.0
+    
     var body: some View {
         VStack {
-            ColorCyclingCircle(amount: colorCycle)
-                .frame(width: 300, height: 300)
+            ColorCyclingRectangle(
+                amount: colorCycle,
+                startX: gradientStartX,
+                startY: gradientStartY,
+                endX: gradientEndX,
+                endY: gradientEndY
+            )
+            .frame(width: 300, height: 300)
 
-            Slider(value: $colorCycle)
+            HStack {
+                Text("Color: ")
+                    .fontWeight(.bold)
+                Slider(value: $colorCycle)
+            }
+            
+            createSeparator()
+            
+            HStack {
+                Text("Gradient's Start Position")
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            
+            HStack {
+                Text("X: ")
+                Slider(value: $gradientStartX, in: 0.0...1.0)
+            }
+            
+            HStack {
+                Text("Y: ")
+                Slider(value: $gradientStartY, in: 0.0...1.0)
+            }
+            
+            createSeparator()
+            
+            HStack {
+                Text("Gradient's End Position")
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            
+            HStack {
+                Text("X: ")
+                Slider(value: $gradientEndX, in: 0.0...1.0)
+            }
+            
+            HStack {
+                Text("Y: ")
+                Slider(value: $gradientEndY, in: 0.0...1.0)
+            }
         }
         
     }
