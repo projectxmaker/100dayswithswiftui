9da9c5d7b7f19c173d802fc8a54e8eea8bf89887
Tuan <projectxmaker@gmail.com>
2022-10-14 17:24:13 +0700

    Challenge 3 of Project 9: clone code of view ColorCyclingCircle

--- Drawing/Drawing/ColorCyclingRetangleView.swift
+++ Drawing/Drawing/ColorCyclingRetangleView.swift
@@ -7,9 +7,53 @@
 
 import SwiftUI
 
-struct ColorCyclingRetangleView: View {
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            ForEach(0..<steps, id: \.self) { value in
+                Circle()
+                    .inset(by: Double(value))
+                    .strokeBorder(
+                        LinearGradient(
+                            gradient: Gradient(colors: [
+                                color(for: value, brightness: 1),
+                                color(for: value, brightness: 0.5)
+                            ]),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        ),
+                        lineWidth: 2
+                    )
+            }
+        }
+        .drawingGroup()
+    }
+
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
+
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
+struct ColorCyclingRetangleView: View {
+    @State private var colorCycle = 0.0
+
+    var body: some View {
+        VStack {
+            ColorCyclingCircle(amount: colorCycle)
+                .frame(width: 300, height: 300)
+
+            Slider(value: $colorCycle)
+        }
+        
     }
 }
 
