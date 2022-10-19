20624f2de44cb385377e2359575a2f467c73d628
Tuan <projectxmaker@gmail.com>
2022-10-19 12:11:25 +0700

    init code for Project 10 - Cupcake Corner

---
+++ Project 10 - Cupcake Corner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Pham Anh Tuan on 10/19/22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
