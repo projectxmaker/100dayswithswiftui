13667fcbd9aa53e298088a9e7dd9d6f87fca66ba
Tuan <projectxmaker@gmail.com>
2022-10-20 22:02:43 +0700

    Init code for Project 11 - Bookworm

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Pham Anh Tuan on 10/20/22.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
