6150867fbaf2003147bf04a4bb81788b5e1289e9
Tuan <projectxmaker@gmail.com>
2022-10-21 21:44:20 +0700

    Init code for project 12 - CoreDataProject

---
+++ Project 12 - CoreDataProject/CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Pham Anh Tuan on 10/21/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
