6c2adc31bdb3cfdef6311d49d4f938969ed5814f
Tuan <projectxmaker@gmail.com>
2022-10-22 05:54:11 +0700

    example of using NSPredicate to filter the results with constraints

--- Project 12 - CoreDataProject/CoreDataProject/ContentView.swift
+++ Project 12 - CoreDataProject/CoreDataProject/ContentView.swift
@@ -4,31 +4,42 @@
 //
 //  Created by Pham Anh Tuan on 10/21/22.
 //
-
+import Foundation
 import SwiftUI
+import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
 
-    @FetchRequest(sortDescriptors: []) var wizards: FetchedResults<Wizard>
+    @FetchRequest(
+        sortDescriptors: [],
+        predicate: NSPredicate(format: "NOT name BEGINSWITH[c] %@", "e")
+    ) var ships: FetchedResults<Ship>
 
     var body: some View {
         VStack {
-            List(wizards, id: \.self) { wizard in
-                Text(wizard.name ?? "Unknown")
+            List(ships, id: \.self) { ship in
+                Text(ship.name ?? "Unknown name")
             }
 
-            Button("Add") {
-                let wizard = Wizard(context: moc)
-                wizard.name = "Harry Potter"
-            }
+            Button("Add Examples") {
+                let ship1 = Ship(context: moc)
+                ship1.name = "Enterprise"
+                ship1.universe = "Star Trek"
 
-            Button("Save") {
-                do {
-                    try moc.save()
-                } catch {
-                    print(error.localizedDescription)
-                }
+                let ship2 = Ship(context: moc)
+                ship2.name = "Defiant"
+                ship2.universe = "Star Trek"
+
+                let ship3 = Ship(context: moc)
+                ship3.name = "Millennium Falcon"
+                ship3.universe = "Star Wars"
+
+                let ship4 = Ship(context: moc)
+                ship4.name = "Executor"
+                ship4.universe = "Star Wars"
+
+                try? moc.save()
             }
         }
     }
