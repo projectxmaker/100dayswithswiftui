18a899b5fd782d98c75f6394c7ff79948ed5b465
Tuan <projectxmaker@gmail.com>
2022-10-23 00:12:08 +0700

    Challenge 1 & 2 of Project 11: improve FilterList to accept a parameter which is an enum identifies the predicate type to be used

--- Project 12 - CoreDataProject/CoreDataProject/ContentView.swift
+++ Project 12 - CoreDataProject/CoreDataProject/ContentView.swift
@@ -8,49 +8,110 @@ import Foundation
 import SwiftUI
 import CoreData
 
+enum FilterComparionTypes: String, CaseIterable {
+    case beginsWith = "Begins with"
+    case like = "Like"
+    case contains = "Contains"
+    case endsWith = "Ends with"
+}
+
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
     @FetchRequest(sortDescriptors: []) var countries: FetchedResults<Country>
 
+    let searchForEntities = ["All", "Country", "Candy"]
+    
+    @State private var filterComparisonType = FilterComparionTypes.beginsWith
+    @State private var filterKeyword = ""
+    @State private var filterForEntity = "All"
+    
     var body: some View {
-        VStack {
-            List {
-                ForEach(countries, id: \.self) { country in
-                    Section(country.wrappedFullName) {
-                        ForEach(country.candyArray, id: \.self) { candy in
-                            Text(candy.wrappedName)
+        NavigationView {
+            GeometryReader { geometry in
+                VStack {
+                    FilteredList(filterKey: "fullName", filterComparisionType: filterComparisonType, filterValue: filterKeyword) { (countries: FetchedResults<Country>) in
+                        ForEach(countries, id: \.self) { country in
+                            Section(country.wrappedFullName) {
+                                ForEach(country.candyArray, id: \.self) { candy in
+                                    Text(candy.wrappedName)
+                                }
+                            }
+                        }
+                    }
+
+                    Form {
+                        Section {
+                            VStack {
+                                HStack {
+                                    Picker("", selection: $filterComparisonType) {
+                                        ForEach(FilterComparionTypes.allCases, id: \.self) { comparionType in
+                                            Text(comparionType.rawValue)
+                                        }
+                                    }
+                                    .overlay {
+                                        HStack {
+                                            Image(systemName: "chevron.up.chevron.down")
+                                                .font(.subheadline)
+                                            Text(filterComparisonType.rawValue)
+                                            Spacer()
+                                        }
+                                        .frame(maxWidth: .infinity)
+                                        .background(.white)
+                                    }
+                                    .frame(width: 150)
+                                    .padding(.trailing, -30)
+                                    
+                                    TextField(text: $filterKeyword) {
+                                        Text("Keyword")
+                                    }
+                                    .textInputAutocapitalization(.never)
+                                }
+                                Picker("", selection: $filterForEntity) {
+                                    ForEach(searchForEntities, id: \.self) { eachEntity in
+                                        Text(eachEntity)
+                                    }
+                                }
+                                .pickerStyle(.segmented)
+                            }
+                        } header: {
+                            Text("Search")
+                        }
+                    }
+                    .frame(maxHeight: geometry.size.height/5)
+                }
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button("Add") {
+                            let candy1 = Candy(context: moc)
+                            candy1.name = "Mars"
+                            candy1.origin = Country(context: moc)
+                            candy1.origin?.shortName = "UK"
+                            candy1.origin?.fullName = "United Kingdom"
+                            
+                            let candy2 = Candy(context: moc)
+                            candy2.name = "KitKat"
+                            candy2.origin = Country(context: moc)
+                            candy2.origin?.shortName = "UK"
+                            candy2.origin?.fullName = "United Kingdom"
+                            
+                            let candy3 = Candy(context: moc)
+                            candy3.name = "Twix"
+                            candy3.origin = Country(context: moc)
+                            candy3.origin?.shortName = "UK"
+                            candy3.origin?.fullName = "United Kingdom"
+                            
+                            let candy4 = Candy(context: moc)
+                            candy4.name = "Toblerone"
+                            candy4.origin = Country(context: moc)
+                            candy4.origin?.shortName = "CH"
+                            candy4.origin?.fullName = "Switzerland"
+                            
+                            try? moc.save()
                         }
                     }
                 }
             }
-
-            Button("Add") {
-                let candy1 = Candy(context: moc)
-                candy1.name = "Mars"
-                candy1.origin = Country(context: moc)
-                candy1.origin?.shortName = "UK"
-                candy1.origin?.fullName = "United Kingdom"
-
-                let candy2 = Candy(context: moc)
-                candy2.name = "KitKat"
-                candy2.origin = Country(context: moc)
-                candy2.origin?.shortName = "UK"
-                candy2.origin?.fullName = "United Kingdom"
-
-                let candy3 = Candy(context: moc)
-                candy3.name = "Twix"
-                candy3.origin = Country(context: moc)
-                candy3.origin?.shortName = "UK"
-                candy3.origin?.fullName = "United Kingdom"
-
-                let candy4 = Candy(context: moc)
-                candy4.name = "Toblerone"
-                candy4.origin = Country(context: moc)
-                candy4.origin?.shortName = "CH"
-                candy4.origin?.fullName = "Switzerland"
-
-                try? moc.save()
-            }        }
+        }
     }
 }
 
--- Project 12 - CoreDataProject/CoreDataProject/FilteredList.swift
+++ Project 12 - CoreDataProject/CoreDataProject/FilteredList.swift
@@ -12,16 +12,20 @@ struct FilteredList<T: NSManagedObject, Content: View>: View {
     @FetchRequest var fetchRequest: FetchedResults<T>
     
     // this is our content closure; we'll call this once for each item in the list
-    let content: (T) -> Content
+    let content: (FetchedResults<T>) -> Content
     
-    init(filterKey: String, filterValue: String, @ViewBuilder content: @escaping (T) -> Content) {
-        _fetchRequest = FetchRequest<T>(sortDescriptors: [], predicate: NSPredicate(format: "%K BEGINSWITH %@", filterKey, filterValue))
+    init(filterKey: String, filterComparisionType: FilterComparionTypes, filterValue: String, @ViewBuilder content: @escaping (FetchedResults<T>) -> Content) {
+        let comparisonType = filterComparisionType.rawValue.replacingOccurrences(of: " ", with: "").uppercased()
+
+        _fetchRequest = FetchRequest<T>(
+            sortDescriptors: [],
+            predicate: NSPredicate(format: "%K \(comparisonType)[cd] %@", filterKey, filterValue))
         self.content = content
     }
     
     var body: some View {
-        List(fetchRequest, id: \.self) { singer in
-            self.content(singer)
+        List {
+            self.content(fetchRequest)
         }
     }
 }
