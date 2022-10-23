6d7af273f8154f822098ddb4b1b5f58d9633b10b
Tuan <projectxmaker@gmail.com>
2022-10-23 17:32:16 +0700

    improve the way of showing/hiding Search panel

--- Project 12 - CoreDataProject/CoreDataProject/ContentView.swift
+++ Project 12 - CoreDataProject/CoreDataProject/ContentView.swift
@@ -28,6 +28,8 @@ struct ContentView: View {
     @State private var filterKeyword = ""
     @State private var filterForEntity = FilterEntityTypes.country
     @State private var sortOrder = SortOrder.forward
+    @State private var showFilterPanel = false
+    @State private var resizeResultList = false
     
     var sortDescriptorsOfCountry: [SortDescriptor<Country>] {
         [
@@ -41,109 +43,55 @@ struct ContentView: View {
             SortDescriptor(\.name, order: sortOrder)
         ]
     }
-    
-    
-    
+
     var body: some View {
         NavigationView {
             GeometryReader { geometry in
-                VStack {
-                    if filterForEntity == .country {
-                        FilteredList(filterKeys: ["fullName", "shortName"], filterComparisionType: filterComparisonType, filterValue: filterKeyword, sortDescriptors: sortDescriptorsOfCountry) { (countries: FetchedResults<Country>) in
-                            ForEach(countries, id: \.self) { country in
-                                Section(country.wrappedFullName) {
-                                    ForEach(country.candyArray, id: \.self) { candy in
-                                        Text(candy.wrappedName)
+                ZStack {
+                    VStack {
+                        VStack {
+                            if filterForEntity == .country {
+                                FilteredList(filterKeys: ["fullName", "shortName"], filterComparisionType: filterComparisonType, filterValue: filterKeyword, sortDescriptors: sortDescriptorsOfCountry) { (countries: FetchedResults<Country>) in
+                                    ForEach(countries, id: \.self) { country in
+                                        Section(country.wrappedFullName) {
+                                            ForEach(country.candyArray, id: \.self) { candy in
+                                                Text(candy.wrappedName)
+                                            }
+                                        }
+                                    }
+                                }
+                            } else {
+                                FilteredList(filterKeys: ["name"], filterComparisionType: filterComparisonType, filterValue: filterKeyword, sortDescriptors: sortDescriptorsOfCandy) { (candies: FetchedResults<Candy>) in
+                                    ForEach(candies, id: \.self) { candy in
+                                        getCandyText(candy: candy)
                                     }
                                 }
                             }
                         }
-                    } else {
-                        FilteredList(filterKeys: ["name"], filterComparisionType: filterComparisonType, filterValue: filterKeyword, sortDescriptors: sortDescriptorsOfCandy) { (candies: FetchedResults<Candy>) in
-                            ForEach(candies, id: \.self) { candy in
-                                getCandyText(candy: candy)
-                            }
-                        }
+                        Spacer(minLength: resizeResultList ? geometry.size.height * 1/3 : 0)
                     }
 
-                    Form {
-                        Section {
-                            VStack {
-                                HStack {
-                                    Picker("", selection: $filterComparisonType) {
-                                        ForEach(FilterComparionTypes.allCases, id: \.self) { comparionType in
-                                            Text(comparionType.rawValue)
-                                        }
-                                    }
-                                    .overlay {
-                                        HStack {
-                                            Image(systemName: "chevron.up.chevron.down")
-                                                .font(.subheadline)
-                                            Text(filterComparisonType.rawValue)
-                                            Spacer()
-                                        }
-                                        .frame(maxWidth: .infinity)
-                                        .background(.white)
-                                    }
-                                    .frame(width: 150)
-                                    .padding(.trailing, -30)
-                                    
-                                    TextField(text: $filterKeyword) {
-                                        Text("Keyword")
-                                    }
-                                    .textInputAutocapitalization(.never)
-                                }
-                                
-                                Picker("", selection: $filterForEntity) {
-                                    ForEach(FilterEntityTypes.allCases, id: \.self) { eachEntity in
-                                        Text(eachEntity.rawValue)
-                                    }
-                                }
-                                .pickerStyle(.segmented)
-                            }
-                        } header: {
-                            Text("Search")
-                        }
-                        
-                        Section("Order by") {
-                            Picker("Order by: ", selection: $sortOrder) {
-                                Text("Accending").tag(SortOrder.forward)
-                                Text("Decreasing").tag(SortOrder.reverse)
-                            }
-                            .pickerStyle(.segmented)
-                        }
+                    VStack {
+                        Spacer()
+                        getSearchPanel(geometry)
                     }
-                    .frame(maxHeight: geometry.size.height/3)
                 }
                 .toolbar {
                     ToolbarItem(placement: .navigationBarTrailing) {
-                        Button("Add") {
-                            let candy1 = Candy(context: moc)
-                            candy1.name = "Mars"
-                            candy1.origin = Country(context: moc)
-                            candy1.origin?.shortName = "UK"
-                            candy1.origin?.fullName = "United Kingdom"
+                        Button("Filter") {
+                            showFilterPanel.toggle()
                             
-                            let candy2 = Candy(context: moc)
-                            candy2.name = "KitKat"
-                            candy2.origin = Country(context: moc)
-                            candy2.origin?.shortName = "UK"
-                            candy2.origin?.fullName = "United Kingdom"
-                            
-                            let candy3 = Candy(context: moc)
-                            candy3.name = "Twix"
-                            candy3.origin = Country(context: moc)
-                            candy3.origin?.shortName = "UK"
-                            candy3.origin?.fullName = "United Kingdom"
-                            
-                            let candy4 = Candy(context: moc)
-                            candy4.name = "Toblerone"
-                            candy4.origin = Country(context: moc)
-                            candy4.origin?.shortName = "CH"
-                            candy4.origin?.fullName = "Switzerland"
-                            
-                            try? moc.save()
+                            if showFilterPanel {
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+                                    resizeResultList.toggle()
+                                }
+                            } else {
+                                resizeResultList.toggle()
+                            }
                         }
+                    }
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        addSamplesButton()
                     }
                 }
             }
@@ -161,6 +109,93 @@ struct ContentView: View {
         return Section(candy.wrappedName) {
             Text(countryInfo)
         }
+    }
+    
+    func addSamplesButton() -> some View {
+        Button("Add") {
+            let candy1 = Candy(context: moc)
+            candy1.name = "Mars"
+            candy1.origin = Country(context: moc)
+            candy1.origin?.shortName = "UK"
+            candy1.origin?.fullName = "United Kingdom"
+            
+            let candy2 = Candy(context: moc)
+            candy2.name = "KitKat"
+            candy2.origin = Country(context: moc)
+            candy2.origin?.shortName = "UK"
+            candy2.origin?.fullName = "United Kingdom"
+            
+            let candy3 = Candy(context: moc)
+            candy3.name = "Twix"
+            candy3.origin = Country(context: moc)
+            candy3.origin?.shortName = "UK"
+            candy3.origin?.fullName = "United Kingdom"
+            
+            let candy4 = Candy(context: moc)
+            candy4.name = "Toblerone"
+            candy4.origin = Country(context: moc)
+            candy4.origin?.shortName = "CH"
+            candy4.origin?.fullName = "Switzerland"
+            
+            try? moc.save()
+        }
+    }
+    
+    func getSearchPanel(_ geometry: GeometryProxy) -> some View {
+        VStack {
+            Form {
+                Section {
+                    VStack {
+                        HStack {
+                            Picker("", selection: $filterComparisonType) {
+                                ForEach(FilterComparionTypes.allCases, id: \.self) { comparionType in
+                                    Text(comparionType.rawValue)
+                                }
+                            }
+                            .overlay {
+                                HStack {
+                                    Image(systemName: "chevron.up.chevron.down")
+                                        .font(.subheadline)
+                                    Text(filterComparisonType.rawValue)
+                                    Spacer()
+                                }
+                                .frame(maxWidth: .infinity)
+                                .background(.white)
+                            }
+                            .frame(width: 150)
+                            .padding(.trailing, -30)
+                            
+                            TextField(text: $filterKeyword) {
+                                Text("Keyword")
+                            }
+                            .textInputAutocapitalization(.never)
+                        }
+                        
+                        Picker("", selection: $filterForEntity) {
+                            ForEach(FilterEntityTypes.allCases, id: \.self) { eachEntity in
+                                Text(eachEntity.rawValue)
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    }
+                } header: {
+                    Text("Search")
+                }
+                
+                Section("Order by") {
+                    Picker("Order by: ", selection: $sortOrder) {
+                        Text("Accending").tag(SortOrder.forward)
+                        Text("Decreasing").tag(SortOrder.reverse)
+                    }
+                    .pickerStyle(.segmented)
+                }
+            }
+        }
+        .frame(maxHeight: showFilterPanel ? geometry.size.height/3 : 0)
+        .opacity(showFilterPanel ? 1 : 0)
+        .transition(.slide)
+        .animation(.easeInOut(duration: 0.5), value: showFilterPanel)
+        .layoutPriority(showFilterPanel ? 1 : 0)
     }
 }
 
