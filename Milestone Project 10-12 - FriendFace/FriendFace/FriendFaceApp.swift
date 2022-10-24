e4eaca42a4b20a430dc47090b31865c706017fbd
Tuan <projectxmaker@gmail.com>
2022-10-24 14:37:11 +0700

    Init code for Milestone Project 10-12

---
+++ Milestone Project 10-12 - FriendFace/FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Pham Anh Tuan on 10/24/22.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
