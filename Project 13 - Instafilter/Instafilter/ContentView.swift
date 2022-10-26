989359a55883015cf470d9854608a99a941e7a15
Tuan <projectxmaker@gmail.com>
2022-10-26 22:32:29 +0700

    learn: integrate PHPickerViewController into SwiftUI View

--- Project 13 - Instafilter/Instafilter/ContentView.swift
+++ Project 13 - Instafilter/Instafilter/ContentView.swift
@@ -6,16 +6,78 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+    @State private var image: Image?
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
+
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        image = Image(uiImage: inputImage)
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            image?
+                .resizable()
+                .scaledToFit()
+
+            Button("Select Image") {
+               showingImagePicker = true
+            }
         }
-        .padding()
+        .sheet(isPresented: $showingImagePicker) {
+            ImagePicker(image: $inputImage)
+        }
+        .onChange(of: inputImage) { _ in loadImage() }
+    }
+}
+
+struct ImagePicker: UIViewControllerRepresentable {
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            // Tell the picker to go away
+            picker.dismiss(animated: true)
+
+            // Exit if no selection was made
+            guard let provider = results.first?.itemProvider else { return }
+
+            // If this has an image we can use, use it
+            if provider.canLoadObject(ofClass: UIImage.self) {
+                provider.loadObject(ofClass: UIImage.self) { image, _ in
+                    self.parent.image = image as? UIImage
+                }
+            }
+        }
+        
+        var parent: ImagePicker
+
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+    }
+    
+    @Binding var image: UIImage?
+
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var config = PHPickerConfiguration()
+        config.filter = .images
+
+        let picker = PHPickerViewController(configuration: config)
+        
+        picker.delegate = context.coordinator
+
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
     }
 }
 
