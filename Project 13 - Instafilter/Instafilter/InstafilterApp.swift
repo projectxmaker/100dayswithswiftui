f103fe60a2b1b18bed5f500da98479d96f536488
Tuan <projectxmaker@gmail.com>
2022-10-26 15:14:58 +0700

    Init code for Project 13

---
+++ Project 13 - Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Pham Anh Tuan on 10/26/22.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
