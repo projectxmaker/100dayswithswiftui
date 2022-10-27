a448d419b13ccbd68194cd12b3dca0093f7617d8
Tuan <projectxmaker@gmail.com>
2022-10-27 18:46:12 +0700

    learn: display MapMaker on Map

--- Project 14 - Bucket List/BucketList/ContentView.swift
+++ Project 14 - Bucket List/BucketList/ContentView.swift
@@ -6,32 +6,27 @@
 //
 
 import SwiftUI
+import MapKit
 
-struct User: Codable {
-    var name: String
+struct Location: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
 }
 
 struct ContentView: View {
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
+    
     var body: some View {
-        Text("Hello World")
-            .onTapGesture {
-                let arrUsers = [
-                    User(name: "Nam"),
-                    User(name: "Phuong"),
-                    User(name: "Bee"),
-                    User(name: "Vy")
-                ]
-                
-                let url = FileManager.default.encodeJSON("users", fileData: arrUsers)
-                
-                if let _ = url {
-                    let users: [User] = FileManager.default.decodeJSON("users")
-                    
-                    print(users)
-                } else {
-                    print("oops")
-                }
-            }
+        //Map(coordinateRegion: $mapRegion)
+        Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+            MapMarker(coordinate: location.coordinate)
+        }
     }
 }
 
