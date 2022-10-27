e018f14e3ffea5d9236de803a083f5ddaa293851
Tuan <projectxmaker@gmail.com>
2022-10-27 11:17:08 +0700

    Challenge 2 project 13: add further sliders for scale and radius

--- Project 13 - Instafilter/Instafilter/ContentView.swift
+++ Project 13 - Instafilter/Instafilter/ContentView.swift
@@ -12,6 +12,8 @@ import CoreImage.CIFilterBuiltins
 struct ContentView: View {
     @State private var image: Image?
     @State private var filterIntensity = 0.5
+    @State private var filterScale = 0.5
+    @State private var filterRadius = 0.5
     @State private var showingImagePicker = false
     @State private var showingFilterSheet = false
     @State private var showingSaveButton = false
@@ -51,11 +53,34 @@ struct ContentView: View {
 
                 HStack {
                     Text("Intensity")
+                        .frame(width: 70, alignment: .leading)
+                    
                     Slider(value: $filterIntensity)
                         .onChange(of: filterIntensity) { _ in
                                 applyProcessing()
                             }
                 }
+                
+                HStack {
+                    Text("Scale")
+                        .frame(width: 70, alignment: .leading)
+                    
+                    Slider(value: $filterScale)
+                        .onChange(of: filterScale) { _ in
+                                applyProcessing()
+                            }
+                }
+                
+                HStack {
+                    Text("Radius")
+                        .frame(width: 70, alignment: .leading)
+                    
+                    Slider(value: $filterRadius)
+                        .onChange(of: filterRadius) { _ in
+                                applyProcessing()
+                            }
+                }
+                
                 .padding(.vertical)
 
                 HStack {
@@ -117,8 +142,8 @@ struct ContentView: View {
         let inputKeys = currentFilter.inputKeys
 
         if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey) }
-        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey) }
-        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterRadius * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterScale * 10, forKey: kCIInputScaleKey) }
 
         guard let outputImage = currentFilter.outputImage else { return }
 
