69831aecc522db361460e023ff61e2cfc24f0acf
Tuan <projectxmaker@gmail.com>
2022-10-28 13:29:20 +0700

    Challenge 1 of Project 14: move modifiers of plus button to image inside it helps the user taps on button more precise

--- Project 14 - Bucket List/BucketList/ContentView.swift
+++ Project 14 - Bucket List/BucketList/ContentView.swift
@@ -46,13 +46,13 @@ struct ContentView: View {
                             viewModel.addLocation()
                         } label: {
                             Image(systemName: "plus")
+                                .padding()
+                                .background(.black.opacity(0.75))
+                                .foregroundColor(.white)
+                                .font(.title)
+                                .clipShape(Circle())
+                                .padding(.trailing)
                         }
-                        .padding()
-                        .background(.black.opacity(0.75))
-                        .foregroundColor(.white)
-                        .font(.title)
-                        .clipShape(Circle())
-                        .padding(.trailing)
                     }
                 }
             } else {
