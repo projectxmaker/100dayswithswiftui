7af8f60ca38de3aead9a26562799ca580b348b8d
Tuan <projectxmaker@gmail.com>
2022-10-29 09:16:39 +0700

    add modifier so that VoiceOver can read about being-shown image

--- Project 15 - Accessibility Feature/AccessibilityFeature/ContentView.swift
+++ Project 15 - Accessibility Feature/AccessibilityFeature/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    let pictures = [
+        "ales-krivec-15949",
+        "galina-n-189483",
+        "kevin-horstmann-141705",
+        "nicolas-tissot-335096"
+    ]
+
+    let labels = [
+        "Tulips",
+        "Frozen tree buds",
+        "Sunflowers",
+        "Fireworks",
+    ]
+    
+    @State private var selectedPicture = Int.random(in: 0...3)
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Image(pictures[selectedPicture])
+            .resizable()
+            .scaledToFit()
+            .onTapGesture {
+                selectedPicture = Int.random(in: 0...3)
+            }
+            .accessibilityLabel(labels[selectedPicture])
+
     }
 }
 
