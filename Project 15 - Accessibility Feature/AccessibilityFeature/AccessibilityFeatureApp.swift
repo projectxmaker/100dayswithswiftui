f30d6fe6313c6a50226975de36e1003bff7e6b0c
Tuan <projectxmaker@gmail.com>
2022-10-29 09:05:50 +0700

    Init code for project 15 - Accessibility Feature

---
+++ Project 15 - Accessibility Feature/AccessibilityFeature/AccessibilityFeatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityFeatureApp.swift
+//  AccessibilityFeature
+//
+//  Created by Pham Anh Tuan on 10/29/22.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityFeatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
