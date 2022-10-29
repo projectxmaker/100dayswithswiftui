a380ec75a52e3b07abe1a9cf4bb4b0cdc8bb2aba
Tuan <projectxmaker@gmail.com>
2022-10-29 09:18:22 +0700

    add modifier so that VoiceOver ignore reading that being-shown image is an image

--- Project 15 - Accessibility Feature/AccessibilityFeature/ContentView.swift
+++ Project 15 - Accessibility Feature/AccessibilityFeature/ContentView.swift
@@ -33,6 +33,7 @@ struct ContentView: View {
             }
             .accessibilityLabel(labels[selectedPicture])
             .accessibilityAddTraits(.isButton)
+            .accessibilityRemoveTraits(.isImage)
 
     }
 }
