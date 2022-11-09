7069dae20b6ae510ab2aa29e9c895c29beae159f
Tuan <projectxmaker@gmail.com>
2022-11-09 08:28:52 +0700

    init code of project 16 - Hot Prospects

---
+++ Project 16 - Hot Prospects/HotProspects/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Pham Anh Tuan on 11/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project 16 - Hot Prospects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Pham Anh Tuan on 11/9/22.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
