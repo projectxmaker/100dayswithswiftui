ae7af42ec66ff2fa595a3c3e1af783f536ba20a9
Tuan <projectxmaker@gmail.com>
2022-11-10 11:52:46 +0700

    save / load contacts in/from UsersDefault, apply access control

--- Project 16 - Hot Prospects/HotProspects/ProspectsView.swift
+++ Project 16 - Hot Prospects/HotProspects/ProspectsView.swift
@@ -52,7 +52,7 @@ struct ProspectsView: View {
             person.name = details[0]
             person.emailAddress = details[1]
 
-            prospects.people.append(person)
+            prospects.add(person)
         case .failure(let error):
             print("Scanning failed: \(error.localizedDescription)")
         }
