b68c919a927dba5e0629fb662a18147f59f038a4
Tuan <projectxmaker@gmail.com>
2022-11-10 08:43:43 +0700

    build tabbar w/ placeholders

--- Project 16 - Hot Prospects/HotProspects/ContentView.swift
+++ Project 16 - Hot Prospects/HotProspects/ContentView.swift
@@ -9,14 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView {
+            ProspectsView()
+                .tabItem {
+                    Label("Everyone", systemImage: "person.3")
+                }
+            ProspectsView()
+                .tabItem {
+                    Label("Contacted", systemImage: "checkmark.circle")
+                }
+            ProspectsView()
+                .tabItem {
+                    Label("Uncontacted", systemImage: "questionmark.diamond")
+                }
+            MeView()
+                .tabItem {
+                    Label("Me", systemImage: "person.crop.square")
+                }
         }
-        .padding()
     }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ Project 16 - Hot Prospects/HotProspects/ProspectsView.swift
@@ -0,0 +1,20 @@
+//
+//  ProspectsView.swift
+//  HotProspects
+//
+//  Created by Pham Anh Tuan on 11/10/22.
+//
+
+import SwiftUI
+
+struct ProspectsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProspectsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProspectsView()
+    }
+}
