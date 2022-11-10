890b3a4676a5f6f7045c3aadb43259f0fae3d6c7
Tuan <projectxmaker@gmail.com>
2022-11-10 09:46:54 +0700

    use filter to get corresponding contacts for each of three tabs

--- Project 16 - Hot Prospects/HotProspects/ProspectsView.swift
+++ Project 16 - Hot Prospects/HotProspects/ProspectsView.swift
@@ -27,20 +27,40 @@ struct ProspectsView: View {
         }
     }
     
+    var filteredProspects: [Prospect] {
+        switch filter {
+        case .none:
+            return prospects.people
+        case .contacted:
+            return prospects.people.filter { $0.isContacted }
+        case .uncontacted:
+            return prospects.people.filter { !$0.isContacted }
+        }
+    }
+    
     var body: some View {
         NavigationView {
-            Text("People: \(prospects.people.count)")
-                .navigationTitle(title)
-                .toolbar {
-                    Button {
-                        let prospect = Prospect()
-                        prospect.name = "Paul Hudson"
-                        prospect.emailAddress = "paul@hackingwithswift.com"
-                        prospects.people.append(prospect)
-                    } label: {
-                        Label("Scan", systemImage: "qrcode.viewfinder")
+            List {
+                ForEach(filteredProspects) { prospect in
+                    VStack(alignment: .leading) {
+                        Text(prospect.name)
+                            .font(.headline)
+                        Text(prospect.emailAddress)
+                            .foregroundColor(.secondary)
                     }
                 }
+            }
+            .navigationTitle(title)
+            .toolbar {
+                Button {
+                    let prospect = Prospect()
+                    prospect.name = "Paul Hudson"
+                    prospect.emailAddress = "paul@hackingwithswift.com"
+                    prospects.people.append(prospect)
+                } label: {
+                    Label("Scan", systemImage: "qrcode.viewfinder")
+                }
+            }
         }
     }
 }
