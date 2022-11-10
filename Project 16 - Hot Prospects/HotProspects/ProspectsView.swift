26c3d301d02174f6b2c102c3136287f183c01427
Tuan <projectxmaker@gmail.com>
2022-11-10 11:05:57 +0700

    import and use CodeScanner to scan the QR code and add new contact

--- Project 16 - Hot Prospects/HotProspects/ProspectsView.swift
+++ Project 16 - Hot Prospects/HotProspects/ProspectsView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import CodeScanner
 
 enum FilterType {
     case none, contacted, uncontacted
@@ -13,6 +14,7 @@ enum FilterType {
 
 struct ProspectsView: View {
     @EnvironmentObject var prospects: Prospects
+    @State private var isShowingScanner = false
 
     let filter: FilterType
     
@@ -38,6 +40,24 @@ struct ProspectsView: View {
         }
     }
     
+    func handleScan(result: Result<ScanResult, ScanError>) {
+        isShowingScanner = false
+        
+        switch result {
+        case .success(let result):
+            let details = result.string.components(separatedBy: "\n")
+            guard details.count == 2 else { return }
+
+            let person = Prospect()
+            person.name = details[0]
+            person.emailAddress = details[1]
+
+            prospects.people.append(person)
+        case .failure(let error):
+            print("Scanning failed: \(error.localizedDescription)")
+        }
+    }
+    
     var body: some View {
         NavigationView {
             List {
@@ -53,13 +73,13 @@ struct ProspectsView: View {
             .navigationTitle(title)
             .toolbar {
                 Button {
-                    let prospect = Prospect()
-                    prospect.name = "Paul Hudson"
-                    prospect.emailAddress = "paul@hackingwithswift.com"
-                    prospects.people.append(prospect)
+                    isShowingScanner = true
                 } label: {
                     Label("Scan", systemImage: "qrcode.viewfinder")
                 }
+            }
+            .sheet(isPresented: $isShowingScanner) {
+                CodeScannerView(codeTypes: [.qr], simulatedData: "Paul Hudson\npaul@hackingwithswift.com", completion: handleScan)
             }
         }
     }
