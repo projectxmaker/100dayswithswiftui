8ccad5c82ff6631840c6fdf31e2b977bb5dcc438
Tuan <projectxmaker@gmail.com>
2022-11-10 12:14:07 +0700

    use contextMenu to save QR code into Photo Library

--- Project 16 - Hot Prospects/HotProspects/MeView.swift
+++ Project 16 - Hot Prospects/HotProspects/MeView.swift
@@ -12,6 +12,7 @@ import CoreImage.CIFilterBuiltins
 struct MeView: View {
     @State private var name = "Anonymous"
     @State private var emailAddress = "you@yoursite.com"
+    @State private var qrCode = UIImage()
     
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
@@ -28,6 +29,10 @@ struct MeView: View {
         return UIImage(systemName: "xmark.circle") ?? UIImage()
     }
     
+    func updateCode() {
+        qrCode = generateQRCode(from: "\(name)\n\(emailAddress)")
+    }
+    
     var body: some View {
         NavigationView {
             Form {
@@ -39,11 +44,22 @@ struct MeView: View {
                     .textContentType(.emailAddress)
                     .font(.title)
                 
-                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                Image(uiImage: qrCode)
                     .interpolation(.none)
                     .resizable()
                     .scaledToFit()
                     .frame(width: 200, height: 200)
+                    .contextMenu {
+                        Button {
+                            let imageSaver = ImageSaver()
+                            imageSaver.writeToPhotoAlbum(image: qrCode)
+                        } label: {
+                            Label("Save to Photos", systemImage: "square.and.arrow.down")
+                        }
+                    }
+                    .onAppear(perform: updateCode)
+                    .onChange(of: name) { _ in updateCode() }
+                    .onChange(of: emailAddress) { _ in updateCode() }
                     
             }
             .navigationTitle("Your code")
