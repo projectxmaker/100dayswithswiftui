268e293fc2923aad157e2242c9c22d59537ae0ac
Tuan <projectxmaker@gmail.com>
2022-11-11 21:40:00 +0700

    init code for project 17 - Flashzilla

---
+++ Project 17 - Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Pham Anh Tuan on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
