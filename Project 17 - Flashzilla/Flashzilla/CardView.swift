ad3315b2dca9f4f1fcb1d4ec8c7bd1923157bf91
Tuan <projectxmaker@gmail.com>
2022-11-12 21:31:07 +0700

    prompt and answer will be replaced each other when VoiceOver turns on

--- Project 17 - Flashzilla/Flashzilla/CardView.swift
+++ Project 17 - Flashzilla/Flashzilla/CardView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct CardView: View {
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @Environment(\.accessibilityVoiceOverEnabled) var voiceOverEnabled
 
     @State private var isShowingAnswer = false
     @State private var offset = CGSize.zero
@@ -35,14 +36,20 @@ struct CardView: View {
                 .shadow(radius: 10)
 
             VStack {
-                Text(card.prompt)
-                    .font(.largeTitle)
-                    .foregroundColor(.black)
+                if voiceOverEnabled {
+                    Text(isShowingAnswer ? card.answer : card.prompt)
+                        .font(.largeTitle)
+                        .foregroundColor(.black)
+                } else {
+                    Text(card.prompt)
+                        .font(.largeTitle)
+                        .foregroundColor(.black)
 
-                if isShowingAnswer {
-                    Text(card.answer)
-                        .font(.title)
-                        .foregroundColor(.gray)
+                    if isShowingAnswer {
+                        Text(card.answer)
+                            .font(.title)
+                            .foregroundColor(.gray)
+                    }
                 }
             }
             .padding(20)
