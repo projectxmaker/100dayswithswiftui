01a13111fbf0e52237e9795298412d6687189f84
Tuan <projectxmaker@gmail.com>
2022-11-12 18:55:55 +0700

    only card at the top is tappable, the others are not tappable

--- Project 17 - Flashzilla/Flashzilla/ContentView.swift
+++ Project 17 - Flashzilla/Flashzilla/ContentView.swift
@@ -61,6 +61,7 @@ struct ContentView: View {
                             }
                         }
                         .stacked(at: index, in: cards.count)
+                        .allowsHitTesting(index == cards.count - 1)
                     }
                 }
                 .allowsHitTesting(timeRemaining > 0)
