ed0a50e9e49d162d446a5ba0da90525cf0cd8a8a
Tuan <projectxmaker@gmail.com>
2022-11-12 22:01:37 +0700

    Challenge 1 of project 17: clear prompt and answer after the card is created

--- Project 17 - Flashzilla/Flashzilla/EditCards.swift
+++ Project 17 - Flashzilla/Flashzilla/EditCards.swift
@@ -69,6 +69,9 @@ struct EditCards: View {
         let card = Card(prompt: trimmedPrompt, answer: trimmedAnswer)
         cards.insert(card, at: 0)
         saveData()
+        
+        newPrompt = ""
+        newAnswer = ""
     }
 
     func removeCards(at offsets: IndexSet) {
