933934417b7b469b93b2805ee6b5b57f96269b9e
Tuan <projectxmaker@gmail.com>
2022-11-13 15:25:35 +0700

    init code for project18 Layout And Geometry

---
+++ Project 18 - Layout And Geometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Pham Anh Tuan on 11/13/22.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
