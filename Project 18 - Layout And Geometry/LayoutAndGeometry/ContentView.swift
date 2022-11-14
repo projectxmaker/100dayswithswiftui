dd5c80997953d5d6b333417a3cf3a2d3a1c998ec
Tuan <projectxmaker@gmail.com>
2022-11-14 11:14:08 +0700

    init code for challenges of project 18

--- Project 18 - Layout And Geometry/LayoutAndGeometry/ContentView.swift
+++ Project 18 - Layout And Geometry/LayoutAndGeometry/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { geo in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(colors[index % 7])
+                            .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                    }
+                    .frame(height: 40)
+                }
+            }
         }
-        .padding()
     }
 }
 
