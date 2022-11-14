5f620b3bbf454cb1fe72d127e06bf77436ccf556
Tuan <projectxmaker@gmail.com>
2022-11-14 21:40:13 +0700

    Challenge 2 of project 18: Make views adjust their scale depending on their vertical position, with views near the bottom being large and views near the top being small. I would suggest going no smaller than 50% of the regular size.

--- Project 18 - Layout And Geometry/LayoutAndGeometry/ContentView.swift
+++ Project 18 - Layout And Geometry/LayoutAndGeometry/ContentView.swift
@@ -10,9 +10,25 @@ import SwiftUI
 struct ContentView: View {
     let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
     let fadeOutFromTopThreshold: CGFloat = 200
+    let scaleUpToBottomThreshold: CGFloat = 2
+    let scaleUpToTopThreshold: CGFloat = 0.5
     
     func getOpacityOfView(yAxis: CGFloat) -> Double {
         return yAxis < fadeOutFromTopThreshold ? yAxis / fadeOutFromTopThreshold : 1
+    }
+    
+    func getScaleOfView(midYAxis: CGFloat, globalMidY: CGFloat) -> CGFloat {
+        let newScaleRate = midYAxis / globalMidY
+
+        var newScale = 1 * newScaleRate
+        
+        if newScale > scaleUpToBottomThreshold {
+            newScale = scaleUpToBottomThreshold
+        } else if newScale < scaleUpToTopThreshold {
+            newScale = scaleUpToTopThreshold
+        }
+        
+        return newScale
     }
     
     var body: some View {
@@ -24,8 +40,9 @@ struct ContentView: View {
                             .font(.title)
                             .frame(maxWidth: .infinity)
                             .background(colors[index % 7])
+                            .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size .height / 2) / 5, axis: (x: 0, y: 1, z: 0))
                             .opacity(getOpacityOfView(yAxis: geo.frame(in: .global).minY))
-                            .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .scaleEffect(getScaleOfView(midYAxis: geo.frame(in: .global).midY, globalMidY: fullView.frame(in: .global).midY))
                     }
                     .frame(height: 40)
                 }
