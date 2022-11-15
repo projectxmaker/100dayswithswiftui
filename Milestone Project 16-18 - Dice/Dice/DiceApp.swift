243f7547f8cc69d33fb64a81edbc8493b4e0673b
Tuan <projectxmaker@gmail.com>
2022-11-15 12:09:25 +0700

    Init code for Milestone Project 16-18 w/ README.md

---
+++ Milestone Project 16-18 - Dice/Dice/DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp.swift
+//  Dice
+//
+//  Created by Pham Anh Tuan on 11/15/22.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
