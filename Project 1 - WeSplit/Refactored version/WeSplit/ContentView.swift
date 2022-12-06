402412d6aadfcdca392a7f48cec821b9382bd928
Tuan <projectxmaker@gmail.com>
2022-12-06 23:11:42 +0700

    refactore project 1: support languages English & Vietnamese

---
+++ Project 1 - WeSplit/Refactored version/WeSplit/ContentView.swift
@@ -0,0 +1,108 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Pham Anh Tuan on 9/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    @Environment(\.locale) var currentLocale
+
+    @FocusState private var amountTextFieldIsFocused: Bool
+    
+    let tipPercentages = 0...100
+    var totalPerPerson: Double {
+        totalAmountOfCheck / Double(numberOfPeople)
+    }
+    
+    var totalAmountOfCheck: Double {
+        let tipAmount = checkAmount * Double(tipPercentage) / 100
+        let grandTotal = tipAmount + checkAmount
+        
+        return grandTotal
+    }
+    
+    var currencyFormatter: FloatingPointFormatStyle<Double>.Currency {
+        var userCurrency: String
+
+        if #available(iOS 16.0, *) {
+            userCurrency = currentLocale.currency?.identifier ?? "USD"
+        } else {
+            userCurrency = currentLocale.currencyCode ?? "USD"
+        }
+        
+        return FloatingPointFormatStyle<Double>.Currency(code: userCurrency, locale: Locale.current)
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: currencyFormatter)
+                        .id(currencyFormatter)
+                        .keyboardType(.decimalPad)
+                        .focused($amountTextFieldIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100, id:\.self) { element in
+                            Text("\(element) people")
+                        }
+                    }
+                }
+
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) { element in
+                            Text(element, format: .percent)
+                        }
+                    }
+                } header: {
+                    Text("How much tip do you want to leave")
+                }
+                
+                Section {
+                    Text(totalAmountOfCheck, format: currencyFormatter)
+                        .foregroundColor(tipPercentage == 0 ? .red : .primary)
+                } header: {
+                    Text("Total amount for the check")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: currencyFormatter)
+                } header: {
+                    Text("Amount per person")
+                }
+            }
+            .navigationTitle("WeSplit")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        amountTextFieldIsFocused = false
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+            
+            ContentView()
+                .environment(\.locale, .init(identifier: "vi_VN")  )
+            
+            ContentView()
+                .environment(\.locale, .init(identifier: "fr_FR"))
+        }
+    }
+}
---
+++ Project 1 - WeSplit/Refactored version/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Pham Anh Tuan on 9/24/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
