39b8f4de378b3f08e54243cc0c4875ba056caddf
Tuan <projectxmaker@gmail.com>
2022-12-10 15:41:58 +0700

    create refactrefactor version of project 8

---
+++ Project 8 - Moonshot/Refactored version/Moonshot/ContentView.swift
@@ -0,0 +1,82 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Pham Anh Tuan on 10/11/22.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    @State private var showingGrid = true
+    
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    let columnsOfGrid = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
+    let columnsOfList = [
+        GridItem(.flexible(minimum: 150))
+    ]
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: showingGrid ? columnsOfGrid : columnsOfList) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            MissionView(mission: mission, astronauts: astronauts)
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                    .padding()
+                                
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundColor(.white)
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                        .foregroundColor(.white.opacity(0.5))
+                                }
+                                .padding(.vertical)
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                                
+                            }
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
+                        }
+                    }
+                }
+                .padding([.horizontal, .bottom])
+            }
+            .navigationTitle("Moonshot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
+            .toolbar {
+                Button {
+                    showingGrid.toggle()
+                } label: {
+                    Text(showingGrid ? "List" : "Grid")
+                }
+
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project 8 - Moonshot/Refactored version/Moonshot/Data Layer/Models/Mission.swift
@@ -0,0 +1,32 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Pham Anh Tuan on 10/12/22.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: Date?
+    let crew: [CrewRole]
+    let description: String
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+
+    var image: String {
+        "apollo\(id)"
+    }
+    
+    var formattedLaunchDate: String {
+        launchDate?.formatted(date: .abbreviated, time: .omitted) ?? "N/A"
+    }
+}
