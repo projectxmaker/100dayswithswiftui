1411871692ffcda6833b50c1d19f2d36ecf4f317
Tuan <projectxmaker@gmail.com>
2022-12-12 20:44:27 +0700

    init project 19

---
+++ Project 19 - SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Pham Anh Tuan on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
